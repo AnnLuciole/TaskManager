2112c19a51edef180a83258d45fa138282137acb
AnnLuciole <annluciole@mail.ru@>
2023-04-16 13:14:16 +0500

    My version of TaskController

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -3,9 +3,10 @@ package com.example.demo.controller;
 import com.example.demo.model.Task;
 import com.example.demo.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class TaskController {
@@ -16,5 +17,37 @@ public class TaskController {
     @PostMapping("/tasks")
     public Task create(@RequestBody Task task) {
         return taskRepository.save(task);
+    }
+
+    @GetMapping("/tasks/{id}")
+    public Task getTask(@PathVariable("id") Long id) {
+        return taskRepository.findById(id).get();
+    }
+
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks() {
+        List<Task> allTasks = new ArrayList<>();
+        taskRepository.findAll().iterator().forEachRemaining(allTasks::add);
+        return allTasks;
+    }
+
+    @PutMapping("/tasks/{id}")
+    public String updateTask(@PathVariable("id") Long id) {
+        if (taskRepository.existsById(id)) {
+            Task updatedTask = taskRepository.findById(id).get();
+            updatedTask.setDone(true);
+            taskRepository.save(updatedTask);
+            return "Task updated";
+        }
+        return "Update failed";
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public String deleteTask(@PathVariable("id") Long id) {
+        if (taskRepository.existsById(id)) {
+            taskRepository.deleteById(id);
+            return "Task deleted";
+        }
+        return "Delete failed";
     }
 }
