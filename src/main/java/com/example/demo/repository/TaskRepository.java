b6fce722ca9c4dd9b799b9bf648cb1c942bdfad5
AnnLuciole <annluciole@mail.ru@>
2023-04-16 09:59:05 +0500

    Initial commit

---
+++ src/main/java/com/example/demo/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Task;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends CrudRepository<Task, Long> {
+
+}
